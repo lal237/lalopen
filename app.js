1be13b4ffa6495dc322d03caa97a9515fcaea13b
Loic Lemongo Azegha <loicazegha@gmail.com>
2026-02-19 10:18:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Devops from MyProject V4 Groupe21!!!');
+  res.send('Hello Devops from Loic groupe 22B');
 });
 
 const PORT = process.env.PORT || 3000;
